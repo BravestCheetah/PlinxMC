19f33cc9a1637a2b3e5e193a8df226a19a7fb6ec
BravestCheetah <luckedev888@outlook.com>
2025-05-20 17:00:50 +0200

    Added basic test functionality
    Added func to install and launch version.
    
    Inputs (test func):
    1 - Install 1.20.4 (for testing)
    2 - Launch 1.20.4 (also for testing)

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import classes.dir_paths
 import classes.installations
 
 
-
 paths = classes.dir_paths.Paths()
 
+if input(":::->  ") == "1":
+    classes.installations.install_version("1.20.4", paths.mcdir)
+else:
+    classes.installations.launch_version("1.20.4", paths.mcdir, mc.utils.generate_test_options())
--- classes/installations.py
+++ classes/installations.py
@@ -1,5 +1,11 @@
-from minecraft_launcher_lib import install
+from minecraft_launcher_lib import install, command
+from subprocess import Popen
 
 #   Usage: install_version("1.21.5", "path/to/version/dir/")
 def install_version(ver: str, path: str) -> None:
     install.install_minecraft_version(ver, path)
+
+#   Usage: launch_version("1.21.5", "path/to/version/dir/", your_options)
+def launch_version(ver: str, mcdir:str, options:dict) -> None:
+    cmd = command.get_minecraft_command(ver, mcdir, options)
+    Popen(cmd)
